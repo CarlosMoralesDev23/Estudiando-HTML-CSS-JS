b77b479840818e664fb47d7585ff9ce5dae01e75
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-10 02:13:23 -0300

    terminada la seccion 2

---
+++ js/Devtalles/01-Fundamentos/2- Depuracion e importaciones/alerts.js
@@ -0,0 +1,12 @@
+alert('Hola Mundo')
+
+
+
+let nombre = prompt("Cual es tu nombre?");
+console.log(`**** ${nombre} ****`)
+
+
+
+
+let respuesta = confirm(`Estas seguro de eliminar esto?`);
+console.log(respuesta)
---
+++ js/Devtalles/01-Fundamentos/2- Depuracion e importaciones/app.js
@@ -0,0 +1,16 @@
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+const saludo = c + d;
+
+let e = 'Hola de nuevo'
+
+console.log('Hola Mundo')
+console.log(e)
+console.log(saludo)
+
+
