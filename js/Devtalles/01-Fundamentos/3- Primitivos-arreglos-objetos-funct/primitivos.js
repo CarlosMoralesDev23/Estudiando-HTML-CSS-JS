ccc70e21fcbbed2accd75480b37566f226d55144
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-10 14:33:40 -0300

    primitivos

---
+++ js/Devtalles/01-Fundamentos/3- Primitivos-arreglos-objetos-funct/primitivos.js
@@ -0,0 +1,36 @@
+
+let nombre = `Peter Parker (soy un string)`;
+console.log( nombre );
+
+nombre  = `Ben Parker (antes era Peter Parker)`;
+console.log(nombre);
+
+
+nombre = `Tia May`;
+console.log( `La variable nombre es un: ${typeof(nombre)}`)
+
+nombre = 123
+console.log( `La variable nombre es un: ${typeof(nombre)}`)
+console.log( `La variable nombre es un: ${typeof(nombre)}`)
+
+let esMarvel = false
+console.log( `La variable esMarvel es un: ${typeof(esMarvel)}`)
+
+let edad = 33;
+console.log( `La variable edad es un: ${typeof(edad)}`)
+
+let superPoder;
+console.log( `La variable superPoder es un: ${typeof(superPoder)}`)
+// este es Undefine porque la variable no tiene ningun tipo de valor
+
+let soyNull = null;
+console.log( `La variable soyNull es un: ${typeof(soyNull)}`)
+
+let symbol1 = Symbol('a')
+let symbol2 = Symbol('a')
+console.log( `La variable symbol1 es un: ${typeof(symbol1)}`)
+console.log( `La variable symbol2 es un: ${typeof(symbol2)}`)
+console.log( symbol1 === symbol2 )
+
+
+
