40fa1b5da30186826d3097cb95f5b3bee03783e8
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-15 10:05:12 -0300

    solucionado el error de insertAdjacement

--- js/DOM CodingTube/23-DOM-insert-atribute/dom.js
+++ js/DOM CodingTube/23-DOM-insert-atribute/dom.js
@@ -3,7 +3,7 @@ let header   = document.createElement( "header" );
 let h1       = document.createElement( "h1" );
 let h2       = document.createElement( "h2" );
 let section  = document.createElement( "section" );
-const parrafos = [ " Párrafo beforebegin ", " Párrafo afterbegin ", " Párrafo beforeend ", " Párrafo afterend " ];
+const parrafos = [" Párrafo beforebegin "," Párrafo afterbegin "," Párrafo beforeend "," Párrafo afterend ",];
 let sectionReference = section;
 body.append( header, section );
 header.append( h1, h2 );
@@ -30,18 +30,13 @@ let parrafo2 = document.getElementById( "parrafo2" );;
 //*parrafo2.classList.add('superParrafo')                 //*PROBANDO; si se agrego como segunda clase.
 sectionReference.insertBefore( parrafo0, parrafo2 );
 
-for (let i = 0; i <5 ; i++) {
-    let parrafo = document.getElementById( `parrafo${i+6}` );
-    ( i === 0 ) ? sectionReference.insertAdjacentElement( "beforebegin", parrafo ) :
-    ( i === 1 ) ? sectionReference.insertAdjacentElement( "afterbegin", parrafo ) : 
-    ( i === 2 ) ? sectionReference.insertAdjacentElement( "beforeend", parrafo ):
-    sectionReference.insertAdjacentElement("afterend", parrafo);
-}
-
+//Agregar segunda clase
 let superParrafos = document.querySelectorAll('p')
 superParrafos = [...superParrafos]
-superParrafos.forEach(p => p.classList.add('superParrafo'))    //! ERROR; no se agrega como segunda clase
+console.log(superParrafos)
+superParrafos.forEach(p => p.classList.add('superParrafo'))
 
-//TODO falta corregir el error de consola
-//TODO falta corregir codigo para saber porque no se agregan las clases
-
+const insert = [ 'beforebegin', 'afterbegin', 'beforeend', 'afterend' ]
+for (let i = 6; i < superParrafos.length; i++) {
+    sectionReference.insertAdjacentElement(`${insert[i-6]}`, superParrafos[i]);   
+}
--- js/DOM CodingTube/25-DOM-Event-listener/dom.js
+++ js/DOM CodingTube/25-DOM-Event-listener/dom.js
@@ -1,48 +1,52 @@
-let body    = document.getElementsByTagName('body');
-let header  = document.createElement('header');
-let h1      = document.createElement('h1');
-let h2      = document.createElement('h2');
-let section = document.createElement('section')
+let body = document.getElementsByTagName("body");
+let header = document.createElement("header");
+let h1 = document.createElement("h1");
+let h2 = document.createElement("h2");
+let section = document.createElement("section");
 h1.innerText = "Event Listener";
 h2.innerText = "Modificando el DOM";
 header.append(h1, h2);
 body[0].append(header, section);
 
-let sectionReference  = section
+let sectionReference = section;
 for (let i = 1; i <= 10; i++) {
     let button = document.createElement("button");
     button.id = `saludoBtn${i}`;
     button.innerText = `Soy el boton ${i}`;
-    sectionReference.append(button)
+    sectionReference.append(button);
 }
 
-let todosAddEventListener = ['click', 'contextmenu', 'mouseover' ]
-let arrayBotones          = document.querySelectorAll('button')
-arrayBotones              = [...arrayBotones]       
+let todosAddEventListener = ["click", "contextmenu", "mouseover"];
+let arrayBotones = document.querySelectorAll("button");
+arrayBotones = [...arrayBotones];
 
-
-for (let i = 1; i <= todosAddEventListener.length; i++) {
-    let boton = document.getElementById(`saludoBtn${i}`);
-    boton.addEventListener(`${todosAddEventListener[i - 1]}`, (function(index) {
-        return ()=>{
-            ( index === 1 ) ? boton.innerHTML = "Ahora soy super Button":
-            ( index === 2 ) ? console.log("Hola desde la consola"):
-            ( index === 3 ) ? console.log("Hola con mouseover"): null
-        }
-    })(i))
-}
-
-
-
-
-
-// for (let i = 1; i < arrayBotones.length; i++) {
-//     let boton = document.getElementById(`saludoBtn${i}`)
-//     boton.addEventListener(`${todosAddEventListener[i-1]}`, () => {
-//         if (i === 1){
-//             boton.innerHTML = "Ahora soy super Button";
-//         }else if ( i === 2 ) {
-//             console.log("Hola desde la consola")
-//         }
-//     });
+// for (let i = 1; i <= todosAddEventListener.length; i++) {
+//     let boton = document.getElementById(`saludoBtn${i}`);
+//     boton.addEventListener(
+//         `${todosAddEventListener[i - 1]}`,
+//         (function (index) {
+//             return () => {
+//                 index === 1
+//                     ? (boton.innerHTML = "Ahora soy super Button")
+//                     : index === 2
+//                     ? console.log("Hola desde la consola")
+//                     : index === 3
+//                     ? console.log("Hola con mouseover")
+//                     : null;
+//             };
+//         })(i)
+//     );
 // }
+
+for (let i = 1; i < arrayBotones.length; i++) {
+    let boton = document.getElementById(`saludoBtn${i}`);
+    boton.addEventListener(`${todosAddEventListener[i - 1]}`, () => {
+        if (i === 1) {
+            boton.innerHTML = "Ahora soy super Button";
+        } else if (i === 2) {
+            console.log("Hola desde la consola");
+        } else if (i === 3) {
+            console.log("Hola Soy MouseOver");
+        }
+    });
+}
