e009002e0f796023bad66a50cb8ad6201bf8cbc9
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-02-15 02:54:00 -0300

    Event Listeners

--- js/DOM CodingTube/25-DOM-Event-listener/dom.js
+++ js/DOM CodingTube/25-DOM-Event-listener/dom.js
@@ -1,40 +1,48 @@
-let body     = document.querySelector( "body" );
-let header   = document.createElement( "header" );
-let h1       = document.createElement( "h1" );
-let h2       = document.createElement( "h2" );
-let section  = document.createElement( "section" );
-let sectionReference = section;
-body.append( header, section );
-header.append( h1, h2 );
-h1.innerText = " Eliminar nodos ";
-h2.innerText = " Modificando el DOM ";
+let body    = document.getElementsByTagName('body');
+let header  = document.createElement('header');
+let h1      = document.createElement('h1');
+let h2      = document.createElement('h2');
+let section = document.createElement('section')
+h1.innerText = "Event Listener";
+h2.innerText = "Modificando el DOM";
+header.append(h1, h2);
+body[0].append(header, section);
 
-for (let i = 0 ; i < 5 ; i++) {
-    let parrafo = document.createElement( "p" );
-    parrafo.id  = `parrafo${i}`;
-    parrafo.classList.add( "parrafos" );
-    let parrafoTexto = document.createTextNode( `Párrafo ${i}` );
-    parrafo.append( parrafoTexto )
-    sectionReference.appendChild( parrafo );
+let sectionReference  = section
+for (let i = 1; i <= 10; i++) {
+    let button = document.createElement("button");
+    button.id = `saludoBtn${i}`;
+    button.innerText = `Soy el boton ${i}`;
+    sectionReference.append(button)
 }
 
-let parrafo0 = document.getElementById( "parrafo0" );
-let parrafo2 = document.getElementById( "parrafo2" );
-let parrafo3 = document.getElementById( "parrafo3" );
-let parrafo5 = document.createElement( "p" );
-parrafo5.id = 'parrafo5'
-parrafo5.innerText = 'Nuevo Párrafo 5'
+let todosAddEventListener = ['click', 'contextmenu', 'mouseover' ]
+let arrayBotones          = document.querySelectorAll('button')
+arrayBotones              = [...arrayBotones]       
 
-section.removeChild(parrafo2)
-parrafo0.remove()
 
-section.replaceChild(parrafo5, parrafo3)
-
-//Event Listener
-parrafo5.addEventListener('click', ()=>{
-    parrafo5.innerHTML = 'Ahora soy SUPER PARRAFO'
-})
-
+for (let i = 1; i <= todosAddEventListener.length; i++) {
+    let boton = document.getElementById(`saludoBtn${i}`);
+    boton.addEventListener(`${todosAddEventListener[i - 1]}`, (function(index) {
+        return ()=>{
+            ( index === 1 ) ? boton.innerHTML = "Ahora soy super Button":
+            ( index === 2 ) ? console.log("Hola desde la consola"):
+            ( index === 3 ) ? console.log("Hola con mouseover"): null
+        }
+    })(i))
+}
 
 
 
+
+
+// for (let i = 1; i < arrayBotones.length; i++) {
+//     let boton = document.getElementById(`saludoBtn${i}`)
+//     boton.addEventListener(`${todosAddEventListener[i-1]}`, () => {
+//         if (i === 1){
+//             boton.innerHTML = "Ahora soy super Button";
+//         }else if ( i === 2 ) {
+//             console.log("Hola desde la consola")
+//         }
+//     });
+// }
